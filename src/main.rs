824783cec49dc6a574671893d466441e63f44abb
jha <haimezjohn@gmail.com>
2024-09-16 21:54:19 +0200

    feat : add first server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MyResponse {
+    status: u32,
+    value: String,
+}
+
+#[get("/")]
+async fn hello_world() -> impl Responder {
+    let response = MyResponse {
+        status: 200,
+        value: String::from("hello world"),
+    };
+
+    HttpResponse::Ok().json(response)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello_world)
+    })
+    .bind(("0.0.0.0", 6000))?
+    .run()
+    .await
 }
