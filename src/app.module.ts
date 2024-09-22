7fa3c3d3ddb2ca57c65752b2ffb5caf922214972
jha <haimezjohn@gmail.com>
2024-09-23 00:30:08 +0200

    init config projet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { PingModule } from './ping/ping.module'
+import { ConfigModule } from '@nestjs/config'
+import { DatabaseModule } from './database/database.module'
+import { DatabaseTestModule } from './database-test/database-test.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({ envFilePath: '.env.dev.local', isGlobal: true }),
+        DatabaseModule,
+        PingModule,
+        DatabaseTestModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
