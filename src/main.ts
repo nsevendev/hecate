33b7f6b0d4dd0ad6089c9665e721a395b395a392
jha <haimezjohn@gmail.com>
2024-09-23 00:31:40 +0200

    feat : middleware logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { LoggerMiddleware } from './logger/logger.middleware'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule)
+    app.use((req, res, next) => new LoggerMiddleware().use(req, res, next))
+    await app.listen(8000)
 }
-bootstrap();
+bootstrap()
