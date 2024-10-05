f143ee2911aac57c57fbae8a58a4510f7e2a0d4f
jha <haimezjohn@gmail.com>
2024-10-05 14:27:07 +0200

    feat(#13) : ajout test fail pour techno

--- src/techno/techno.module.spec.ts
+++ src/techno/techno.module.spec.ts
@@ -6,9 +6,11 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Test, TestingModule } from '@nestjs/testing'
 import { CreateTechnoDto } from './app/create-techno.dto'
 import { DataSource } from 'typeorm'
+import { ProjectService } from '../project/app/project.service'
 
 describe('TechnoModule', () => {
     let technoService: TechnoService
+    let projectService: ProjectService
     let technoRepository: TechnoRepository
     let module: TestingModule
     let createTechnoDto: CreateTechnoDto
@@ -20,11 +22,12 @@ describe('TechnoModule', () => {
                 DatabaseTestModule, // Utilisation bdd pour les tests
                 TypeOrmModule.forFeature([Techno]),
             ],
-            providers: [TechnoService, TechnoRepository],
+            providers: [TechnoService, TechnoRepository, ProjectService],
         }).compile()
 
         technoService = module.get<TechnoService>(TechnoService)
         technoRepository = module.get<TechnoRepository>(TechnoRepository)
+        projectService = module.get<ProjectService>(ProjectService)
 
         createTechnoDto = { name: 'angular' }
     })
@@ -52,6 +55,13 @@ describe('TechnoModule', () => {
             expect(technos).toEqual([])
         })
 
+        it('TechnoService.getTechno avec des technos', async () => {
+            const technoCreated = await technoService.createTechno(createTechnoDto)
+            const technos = await technoService.getTechnos()
+
+            expect(technos).toEqual([technoCreated])
+        })
+
         it('TechnoService.createTechno sans project', async () => {
             const technoCreated = await technoService.createTechno(createTechnoDto)
 
@@ -59,11 +69,61 @@ describe('TechnoModule', () => {
             expect(technoCreated.name).toBe(createTechnoDto.name)
         })
 
-        it('TechnoService.getTechno avec des technos', async () => {
+        it('TechnoService.updateTechno modifie les propriétés', async () => {
             const technoCreated = await technoService.createTechno(createTechnoDto)
+
+            const updatedTechnoDto = { name: 'UpdatedAngular' }
+            const updatedTechno = await technoService.updateTechno(technoCreated.id, updatedTechnoDto)
+
+            // TODO : Vérifier que la techno a bien été mise à jour
+            expect(updatedTechno.id).toBe(technoCreated.id)
+            expect(updatedTechno.name).toBe(updatedTechnoDto.name)
+        })
+
+        it("TechnoService.updateTechno lance une erreur si la techno n'existe pas", async () => {
+            const nonExistentId = 999
+            const updatedTechnoDto = { name: 'NonExistentTechno' }
+
+            // TODO : Vérifier que la fonction updateTechno lance une erreur si la techno n'existe pas
+            await expect(technoService.updateTechno(nonExistentId, updatedTechnoDto)).rejects.toThrowError(
+                `Techno with ID ${nonExistentId} not found`
+            )
+        })
+
+        it('TechnoService.deleteTechno supprime une techno', async () => {
+            const technoCreated = await technoService.createTechno(createTechnoDto)
+
+            await technoService.deleteTechno(technoCreated.id)
+
             const technos = await technoService.getTechnos()
 
-            expect(technos).toEqual([technoCreated])
+            // TODO : Vérification que la techno n'existe plus
+            expect(technos).toHaveLength(0)
+        })
+
+        it('TechnoService.deleteTechno supprime une techno et met à jour les projets associés', async () => {
+            // Création d'une techno
+            const technoCreated = await technoService.createTechno(createTechnoDto)
+
+            // Création d'un projet associé à la techno
+            const createProjectDto = {
+                name: 'my projet',
+                description: 'super projet',
+                technos: [technoCreated.id],
+            }
+            const projectCreated = await projectService.createProject(createProjectDto)
+
+            await technoService.deleteTechno(technoCreated.id)
+
+            const technos = await technoService.getTechnos()
+
+            // TODO : Vérification que la techno n'existe plus
+            expect(technos).toHaveLength(0)
+
+            const updatedProject = await projectService.getProjectById(projectCreated.id)
+
+            // TODO : Vérification que le projet n'a plus de techno associée
+            expect(updatedProject.technos).toHaveLength(0)
         })
     })
 })
