1a58b42551d5b6fd246d13d87ce12c741dd0cdc3
Aurelien <mosini.aurelien@gmail.com>
2024-10-09 23:02:51 +0200

    create serviceService, serviceController, create and update dto and start creation of service test

---
+++ src/service/service.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Service } from './domaine/service.entity'
+import { ServiceService } from './app/service.service'
+import { ServiceRepository } from './infra/service.repository'
+import { ImageModule } from 'src/image/image.module'
+import { ServiceController } from './app/service.controller'
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Service]), ImageModule],
+    controllers: [ServiceController],
+    providers: [ServiceService, ServiceRepository],
+    exports: [ServiceService, ServiceRepository],
+})
+export class ServiveModule {}
