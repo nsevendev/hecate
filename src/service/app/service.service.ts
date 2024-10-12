256cdf1798c05d91e26b9dc3d378629071411cb8
jha <haimezjohn@gmail.com>
2024-10-12 06:51:30 +0200

    fix : probleme de path des imports, remise à la normal

--- src/service/app/service.service.ts
+++ src/service/app/service.service.ts
@@ -1,35 +1,31 @@
 import { Injectable } from '@nestjs/common'
-import { BaseService } from 'src/shared/base-service/base.service'
+import { BaseService } from '../../shared/base-service/base.service'
 import { ServiceRepository } from '../infra/service.repository'
-import { ImageService } from 'src/image/app/image.service'
+import { ImageService } from '../../image/app/image.service'
 import { InjectRepository } from '@nestjs/typeorm'
-import { Image } from 'src/image/domaine/image.entity'
+import { Image } from '../../image/domaine/image.entity'
 import { CreateServiceDto } from './create-service.dto'
 import { UpdateServiceDto } from './update-service.dto'
 
 @Injectable()
 export class ServiceService extends BaseService {
-    constructor(
-        private readonly serviceRepository: ServiceRepository,
-        @InjectRepository(Image)
-        private readonly imageService: ImageService
-    ) {
-        super('ServiceService')
-    }
+  constructor(
+    private readonly serviceRepository: ServiceRepository,
+    @InjectRepository(Image)
+    private readonly imageService: ImageService
+  ) {
+    super('ServiceService')
+  }
 
-    getServices = async () => {}
+  getServices = async () => {}
 
-    getServiceById = async (id: number) => {}
+  getServiceById = async (id: number) => {}
 
-    createService = async (createServiceDto: CreateServiceDto, file: Express.Multer.File) => {}
+  createService = async (createServiceDto: CreateServiceDto, file: Express.Multer.File) => {}
 
-    updateServiceById = async (
-        id: number,
-        updatedServiceDto: UpdateServiceDto,
-        file: Express.Multer.File
-    ) => {}
+  updateServiceById = async (id: number, updatedServiceDto: UpdateServiceDto, file: Express.Multer.File) => {}
 
-    removeImageFromService = async () => {}
+  removeImageFromService = async () => {}
 
-    deleteProjectById = async () => {}
+  deleteProjectById = async () => {}
 }
--- src/service/service.module.spec.ts
+++ src/service/service.module.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { ServiceService } from './app/service.service'
 import { ServiceController } from './app/service.controller'
-import { DatabaseTestModule } from 'src/database-test/database-test.module'
+import { DatabaseTestModule } from '../database-test/database-test.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Service } from './domaine/service.entity'
 import { ImageModule } from '@faker-js/faker/.'
@@ -9,69 +9,69 @@ import { ServiceRepository } from './infra/service.repository'
 import { CreateServiceDto } from './app/create-service.dto'
 
 describe('ServiceModule', () => {
-    let module: TestingModule
-    let serviceService: ServiceService
-    let serviceController: ServiceController
-    let createServiceDto: CreateServiceDto
-    let mockFile: (filename: string) => Express.Multer.File
+  let module: TestingModule
+  let serviceService: ServiceService
+  let serviceController: ServiceController
+  let createServiceDto: CreateServiceDto
+  let mockFile: (filename: string) => Express.Multer.File
 
-    beforeAll(async () => {
-        module = await Test.createTestingModule({
-            imports: [DatabaseTestModule, TypeOrmModule.forFeature([Service]), ImageModule],
-            controllers: [ServiceController],
-            providers: [ServiceService, ServiceRepository],
-        }).compile()
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [DatabaseTestModule, TypeOrmModule.forFeature([Service]), ImageModule],
+      controllers: [ServiceController],
+      providers: [ServiceService, ServiceRepository],
+    }).compile()
 
-        serviceService = module.get<ServiceService>(ServiceService)
-        serviceController = module.get<ServiceController>(ServiceController)
+    serviceService = module.get<ServiceService>(ServiceService)
+    serviceController = module.get<ServiceController>(ServiceController)
 
-        mockFile = (filename: string): Express.Multer.File => ({
-            fieldname: 'images',
-            originalname: filename,
-            encoding: '7bit',
-            mimetype: 'image/png',
-            buffer: Buffer.from('mocked image content'),
-            size: 1024,
-            destination: 'uploads/',
-            filename,
-            path: `uploads/${filename}`,
-            stream: undefined,
-        })
+    mockFile = (filename: string): Express.Multer.File => ({
+      fieldname: 'images',
+      originalname: filename,
+      encoding: '7bit',
+      mimetype: 'image/png',
+      buffer: Buffer.from('mocked image content'),
+      size: 1024,
+      destination: 'uploads/',
+      filename,
+      path: `uploads/${filename}`,
+      stream: undefined,
     })
+  })
 
-    describe('Service', () => {
-        it('ServiceService est défini', () => {
-            expect(serviceService).toBeDefined()
-        })
-        it('Service.getServices sans service : retourne tableau vide', async () => {
-            const services = await serviceService.getServices()
-
-            expect(services).toEqual([])
-        })
-        it('ServiceService.createService sans image : creer le service', async () => {
-            createServiceDto = {
-                title: 'creation application',
-                description: 'on fait de supers applications',
-            }
-            const serviceCreated = await serviceService.createService(createServiceDto, null)
-
-            expect(serviceCreated.id).toBeDefined()
-            expect(serviceCreated.title).toEqual(createServiceDto.title)
-            expect(serviceCreated.description).toEqual(createServiceDto.description)
-            expect(serviceCreated).not.toHaveProperty('image')
-        })
-        it('ServiceService.createService avec image: creer le service et image', async () => {
-            createServiceDto = {
-                title: 'creation application',
-                description: 'on fait de supers applications',
-            }
-            const file: Express.Multer.File = mockFile('image1.png')
-            const serviceCreated = await serviceService.createService(createServiceDto, file)
-
-            expect(serviceCreated.id).toBeDefined()
-            expect(serviceCreated.title).toEqual(createServiceDto.title)
-            expect(serviceCreated.description).toEqual(createServiceDto.description)
-            expect(serviceCreated.image).toBeDefined()
-        })
+  describe('Service', () => {
+    it('ServiceService est défini', () => {
+      expect(serviceService).toBeDefined()
     })
+    it('Service.getServices sans service : retourne tableau vide', async () => {
+      const services = await serviceService.getServices()
+
+      expect(services).toEqual([])
+    })
+    it('ServiceService.createService sans image : creer le service', async () => {
+      createServiceDto = {
+        title: 'creation application',
+        description: 'on fait de supers applications',
+      }
+      const serviceCreated = await serviceService.createService(createServiceDto, null)
+
+      expect(serviceCreated.id).toBeDefined()
+      expect(serviceCreated.title).toEqual(createServiceDto.title)
+      expect(serviceCreated.description).toEqual(createServiceDto.description)
+      expect(serviceCreated).not.toHaveProperty('image')
+    })
+    it('ServiceService.createService avec image: creer le service et image', async () => {
+      createServiceDto = {
+        title: 'creation application',
+        description: 'on fait de supers applications',
+      }
+      const file: Express.Multer.File = mockFile('image1.png')
+      const serviceCreated = await serviceService.createService(createServiceDto, file)
+
+      expect(serviceCreated.id).toBeDefined()
+      expect(serviceCreated.title).toEqual(createServiceDto.title)
+      expect(serviceCreated.description).toEqual(createServiceDto.description)
+      expect(serviceCreated.image).toBeDefined()
+    })
+  })
 })
