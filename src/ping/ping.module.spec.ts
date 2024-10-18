8c897339fa61ecf65d35f26b0221d6e54abeb9ba
jha <haimezjohn@gmail.com>
2024-10-18 19:26:19 +0200

    feat(#13) : add test for ping module

--- src/ping/ping.module.spec.ts
+++ src/ping/ping.module.spec.ts
@@ -7,90 +7,110 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Ping } from './domaine/ping.entity'
 import { CreatePingDto } from './app/create-ping.dto'
 import { ConflictException, NotFoundException } from '@nestjs/common'
+import { PingModule } from './ping.module'
 
-describe('PingModule', () => {
-    let module: TestingModule
-    let pingController: PingController
-    let pingService: PingService
-    let createPingDto: CreatePingDto
+describe('Ping', () => {
+  let module: TestingModule
+  let pingController: PingController
+  let pingService: PingService
+  let createPingDto: CreatePingDto
 
-    beforeEach(async () => {
-        module = await Test.createTestingModule({
-            imports: [
-                DatabaseTestModule, // Utilisation bdd pour les tests
-                TypeOrmModule.forFeature([Ping]),
-            ],
-            controllers: [PingController],
-            providers: [PingService, PingRepository],
-        }).compile()
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        DatabaseTestModule, // Utilisation bdd pour les tests
+        TypeOrmModule.forFeature([Ping]),
+      ],
+      controllers: [PingController],
+      providers: [PingService, PingRepository],
+    }).compile()
 
-        pingController = module.get<PingController>(PingController)
-        pingService = module.get<PingService>(PingService)
+    pingController = module.get<PingController>(PingController)
+    pingService = module.get<PingService>(PingService)
 
-        createPingDto = { status: 200, value: 'value' }
+    createPingDto = { status: 200, value: 'value' }
+  })
+
+  describe('PingModule', () => {
+    let modulePing: TestingModule
+
+    beforeAll(async () => {
+      modulePing = await Test.createTestingModule({
+        imports: [DatabaseTestModule, PingModule],
+      }).compile()
     })
 
-    describe('Controller', () => {
-        it('PingController est defini', () => {
-            expect(pingController).toBeDefined()
-        })
-
-        it('PingController.firstPing avec aucun ping existant', async () => {
-            await expect(pingController.firstPing()).rejects.toThrow(NotFoundException)
-        })
-
-        it('PingController.createPing', async () => {
-            const result = await pingController.createPing(createPingDto)
-
-            expect(result.status).toEqual(200)
-            expect(result.value).toEqual('value')
-        })
-
-        it('PingController.firstPing', async () => {
-            await pingController.createPing(createPingDto)
-
-            const result = await pingController.firstPing()
-
-            expect(result.status).toEqual(200)
-            expect(result.value).toEqual('value')
-        })
-
-        it('PingController.createPing avec ping qui exist deja', async () => {
-            await pingController.createPing(createPingDto)
-
-            await expect(pingController.createPing(createPingDto)).rejects.toThrow(ConflictException)
-        })
+    it('PingModule es tdefini', () => {
+      expect(modulePing).toBeDefined()
     })
 
-    describe('Service', () => {
-        it('PingService est defini', () => {
-            expect(pingService).toBeDefined()
-        })
-
-        it('PingService.getFirstPing avec aucun ping existant', async () => {
-            await expect(pingService.getFirstPing()).rejects.toThrow(NotFoundException)
-        })
-
-        it('PingService.createPing', async () => {
-            const result = await pingService.createPing(createPingDto)
-
-            expect(result.status).toEqual(200)
-            expect(result.value).toEqual('value')
-        })
-
-        it('PingService.firstPing', async () => {
-            await pingService.createPing(createPingDto)
-
-            const result = await pingService.getFirstPing()
-
-            expect(result.status).toEqual(200)
-            expect(result.value).toEqual('value')
-        })
-
-        it('PingService.createPing avec ping qui exist deja', async () => {
-            await pingService.createPing(createPingDto)
-
-            await expect(pingService.createPing(createPingDto)).rejects.toThrow(ConflictException)
-        })
+    it('PingModule : service et controller son defini', () => {
+      expect(modulePing.get<PingService>(PingService)).toBeDefined()
+      expect(modulePing.get<PingController>(PingController)).toBeDefined()
     })
+  })
+
+  describe('Controller', () => {
+    it('PingController est defini', () => {
+      expect(pingController).toBeDefined()
+    })
+
+    it('PingController.firstPing avec aucun ping existant', async () => {
+      await expect(pingController.firstPing()).rejects.toThrow(NotFoundException)
+    })
+
+    it('PingController.createPing', async () => {
+      const result = await pingController.createPing(createPingDto)
+
+      expect(result.status).toEqual(200)
+      expect(result.value).toEqual('value')
+    })
+
+    it('PingController.firstPing', async () => {
+      await pingController.createPing(createPingDto)
+
+      const result = await pingController.firstPing()
+
+      expect(result.status).toEqual(200)
+      expect(result.value).toEqual('value')
+    })
+
+    it('PingController.createPing avec ping qui exist deja', async () => {
+      await pingController.createPing(createPingDto)
+
+      await expect(pingController.createPing(createPingDto)).rejects.toThrow(ConflictException)
+    })
+  })
+
+  describe('Service', () => {
+    it('PingService est defini', () => {
+      expect(pingService).toBeDefined()
+    })
+
+    it('PingService.getFirstPing avec aucun ping existant', async () => {
+      await expect(pingService.getFirstPing()).rejects.toThrow(NotFoundException)
+    })
+
+    it('PingService.createPing', async () => {
+      const result = await pingService.createPing(createPingDto)
+
+      expect(result.status).toEqual(200)
+      expect(result.value).toEqual('value')
+    })
+
+    it('PingService.firstPing', async () => {
+      await pingService.createPing(createPingDto)
+
+      const result = await pingService.getFirstPing()
+
+      expect(result.status).toEqual(200)
+      expect(result.value).toEqual('value')
+    })
+
+    it('PingService.createPing avec ping qui exist deja', async () => {
+      await pingService.createPing(createPingDto)
+
+      await expect(pingService.createPing(createPingDto)).rejects.toThrow(ConflictException)
+    })
+  })
 })
