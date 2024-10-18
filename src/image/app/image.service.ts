becf185c64021306eb46c71b30682e50d0289084
jha <haimezjohn@gmail.com>
2024-10-18 19:23:19 +0200

    update(#13) : linter image module

--- src/image/app/image.service.ts
+++ src/image/app/image.service.ts
@@ -6,64 +6,65 @@ import { AwsS3Service, fileSchema } from '../../aws/aws-s3.service'
 
 @Injectable()
 export class ImageService extends BaseService {
-    constructor(
-        private readonly imageRepository: ImageRepository,
-        private readonly awsS3Service: AwsS3Service
-    ) {
-        super('ImageService')
+  constructor(
+    private readonly imageRepository: ImageRepository,
+    private readonly awsS3Service: AwsS3Service
+  ) {
+    super('ImageService')
+  }
+
+  getImages = async () => {
+    return await this.imageRepository.repository.find()
+  }
+
+  createImages = async (files: Array<Express.Multer.File>): Promise<Image[]> => {
+    const images: Image[] = []
+
+    for (const file of files) {
+      const validatedFile = fileSchema.parse({
+        size: file.size,
+        buffer: file.buffer,
+        originalname: file.originalname,
+        mimetype: file.mimetype,
+      })
+
+      const s3UploadResult = await this.awsS3Service.uploadFile({ file: validatedFile })
+
+      const image = this.imageRepository.repository.create({ path: s3UploadResult.fileKey })
+      images.push(await this.imageRepository.repository.save(image))
     }
 
-    getImages = async () => {
-        return await this.imageRepository.repository.find()
+    return images
+  }
+
+  deleteImageFromStorage = async (image: Image) => {
+    if (!image.path) {
+      throw new BadRequestException("L'image n'a pas de chemin valide pour la suppression")
     }
 
-    createImages = async (files: Array<Express.Multer.File>): Promise<Image[]> => {
-        const images: Image[] = []
+    await this.awsS3Service.deleteFile({ fileKey: image.path })
+  }
 
-        for (const file of files) {
-            const validatedFile = fileSchema.parse({
-                size: file.size,
-                buffer: file.buffer,
-                originalname: file.originalname,
-                mimetype: file.mimetype,
-            })
+  // TODO : peut être à supprimer si pas d'utilité
+  deleteImageById = async (id: number) => {
+    const image = await this.imageRepository.repository.findOne({ where: { id } })
 
-            const s3UploadResult = await this.awsS3Service.uploadFile({ file: validatedFile })
-
-            const image = this.imageRepository.repository.create({ path: s3UploadResult.fileKey })
-            images.push(await this.imageRepository.repository.save(image))
-        }
-
-        return images
+    if (!image) {
+      throw new NotFoundException("L'Image n'existe pas")
     }
 
-    deleteImageFromStorage = async (image: Image) => {
-        if (!image.path) {
-            throw new BadRequestException("L'image n'a pas de chemin valide pour la suppression")
-        }
+    return await this.imageRepository.repository.remove(image)
+  }
 
-        await this.awsS3Service.deleteFile({ fileKey: image.path })
+  deleteImageByIdAndDeleteFromStorage = async (id: number) => {
+    const image = await this.imageRepository.repository.findOne({ where: { id } })
+
+    if (!image) {
+      throw new NotFoundException("L'Image n'existe pas")
     }
 
-    deleteImageById = async (id: number) => {
-        const image = await this.imageRepository.repository.findOne({ where: { id } })
+    await this.deleteImageFromStorage(image)
 
-        if (!image) {
-            throw new NotFoundException("L'Image n'existe pas")
-        }
-
-        return await this.imageRepository.repository.remove(image)
-    }
-
-    deleteImageByIdAndDeleteFromStorage = async (id: number) => {
-        const image = await this.imageRepository.repository.findOne({ where: { id } })
-
-        if (!image) {
-            throw new NotFoundException("L'Image n'existe pas")
-        }
-
-        await this.deleteImageFromStorage(image)
-
-        return await this.imageRepository.repository.remove(image)
-    }
+    return await this.imageRepository.repository.remove(image)
+  }
 }
--- src/image/image.module.spec.ts
+++ src/image/image.module.spec.ts
@@ -7,74 +7,74 @@ import { ImageRepository } from './infra/image.repository'
 import { AwsS3Service } from '../aws/aws-s3.service'
 
 describe('ImageModule', () => {
-    let module: TestingModule
-    let imageService: ImageService
-    let mockFile: (filename: string) => Express.Multer.File
+  let module: TestingModule
+  let imageService: ImageService
+  let mockFile: (filename: string) => Express.Multer.File
 
-    beforeEach(async () => {
-        module = await Test.createTestingModule({
-            imports: [
-                DatabaseTestModule, // Utilisation bdd pour les tests
-                TypeOrmModule.forFeature([Image]),
-            ],
-            providers: [ImageService, ImageRepository, AwsS3Service],
-        }).compile()
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        DatabaseTestModule, // Utilisation bdd pour les tests
+        TypeOrmModule.forFeature([Image]),
+      ],
+      providers: [ImageService, ImageRepository, AwsS3Service],
+    }).compile()
 
-        imageService = module.get<ImageService>(ImageService)
+    imageService = module.get<ImageService>(ImageService)
 
-        mockFile = (filename: string): Express.Multer.File => ({
-            fieldname: 'images',
-            originalname: filename,
-            encoding: '7bit',
-            mimetype: 'image/png',
-            buffer: Buffer.from('mocked image content'),
-            size: 1024,
-            destination: 'uploads/',
-            filename,
-            path: `uploads/${filename}`,
-            stream: undefined,
-        })
+    mockFile = (filename: string): Express.Multer.File => ({
+      fieldname: 'images',
+      originalname: filename,
+      encoding: '7bit',
+      mimetype: 'image/png',
+      buffer: Buffer.from('mocked image content'),
+      size: 1024,
+      destination: 'uploads/',
+      filename,
+      path: `uploads/${filename}`,
+      stream: undefined,
+    })
+  })
+
+  describe('Service', () => {
+    it('ImageService est defini', () => {
+      expect(imageService).toBeDefined()
     })
 
-    describe('Service', () => {
-        it('ImageService est defini', () => {
-            expect(imageService).toBeDefined()
-        })
+    it('ImageService.getImages sans image : retourne tableau vide', async () => {
+      const images = await imageService.getImages()
 
-        it('ImageService.getImages sans image : retourne tableau vide', async () => {
-            const images = await imageService.getImages()
-
-            expect(images).toEqual([])
-        })
-
-        it('ImageService.getImages avec image : recupere toutes les images', async () => {
-            const imageCreated = await imageService.createImages([mockFile('image.png')])
-            const images = await imageService.getImages()
-
-            expect(images).toEqual(imageCreated)
-
-            // suppression de l'image dans le S3 pour non stockage des test
-            await imageService.deleteImageFromStorage(imageCreated[0])
-        })
-
-        it('ImageService.createImages avec 1 image mocker : creer image dans BDD + S3', async () => {
-            const imageCreated = await imageService.createImages([mockFile('image.png')])
-
-            expect(imageCreated[0].id).toEqual(1)
-
-            // suppression de l'image dans le S3 pour non stockage des test
-            await imageService.deleteImageFromStorage(imageCreated[0])
-        })
-
-        it('ImageService.deleteImage avec 1 image mocker : image supprimer de la BDD + S3', async () => {
-            const imageCreated = await imageService.createImages([mockFile('image.png')])
-            const imageDeleted = await imageService.deleteImageByIdAndDeleteFromStorage(imageCreated[0].id)
-
-            const images = await imageService.getImages()
-
-            // undefined car l'image a été supprimée
-            expect(imageDeleted.id).toEqual(undefined)
-            expect(images).toEqual([])
-        })
+      expect(images).toEqual([])
     })
+
+    it('ImageService.getImages avec image : recupere toutes les images', async () => {
+      const imageCreated = await imageService.createImages([mockFile('image.png')])
+      const images = await imageService.getImages()
+
+      expect(images).toEqual(imageCreated)
+
+      // suppression de l'image dans le S3 pour non stockage des test
+      await imageService.deleteImageFromStorage(imageCreated[0])
+    })
+
+    it('ImageService.createImages avec 1 image mocker : creer image dans BDD + S3', async () => {
+      const imageCreated = await imageService.createImages([mockFile('image.png')])
+
+      expect(imageCreated[0].id).toEqual(1)
+
+      // suppression de l'image dans le S3 pour non stockage des test
+      await imageService.deleteImageFromStorage(imageCreated[0])
+    })
+
+    it('ImageService.deleteImage avec 1 image mocker : image supprimer de la BDD + S3', async () => {
+      const imageCreated = await imageService.createImages([mockFile('image.png')])
+      const imageDeleted = await imageService.deleteImageByIdAndDeleteFromStorage(imageCreated[0].id)
+
+      const images = await imageService.getImages()
+
+      // undefined car l'image a été supprimée
+      expect(imageDeleted.id).toEqual(undefined)
+      expect(images).toEqual([])
+    })
+  })
 })
