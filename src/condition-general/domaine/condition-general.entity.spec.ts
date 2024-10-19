f320cf9f564e4f72da62c2c51ba064c582fcf983
jha <haimezjohn@gmail.com>
2024-10-19 09:08:18 +0200

    update(13) : create entity CG + test OK

--- src/condition-general/domaine/condition-general.entity.spec.ts
+++ src/condition-general/domaine/condition-general.entity.spec.ts
@@ -1,7 +1,16 @@
 import { ConditionGeneral } from './condition-general.entity'
 
 describe('ConditionGeneral', () => {
-  it('should be defined', () => {
+  it('ConditionGeneral : est defini', () => {
     expect(new ConditionGeneral()).toBeDefined()
+  })
+
+  it("ConditionGeneral : creation de l'objet condition general et test les valeurs", () => {
+    const cg = new ConditionGeneral()
+    cg.title = 'titre'
+    cg.intro = 'intro'
+
+    expect(cg.title).toBe('titre')
+    expect(cg.intro).toBe('intro')
   })
 })
--- src/condition-general/domaine/condition-general.entity.ts
+++ src/condition-general/domaine/condition-general.entity.ts
@@ -1,9 +1,26 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
 import { ApiProperty } from '@nestjs/swagger'
+import { ArticleCg } from '../../article-cg/domaine/article-cg.entity'
 
-@Entity()
+@Entity('condition_general')
 export class ConditionGeneral {
   @PrimaryGeneratedColumn()
   @ApiProperty({ description: 'id de la condition general' })
   id: number
+
+  @Column()
+  @ApiProperty({ description: 'Titre de la condition general' })
+  title: string
+
+  @Column()
+  @ApiProperty({ description: 'Sous titre de la condition genereal' })
+  intro?: string
+
+  @OneToMany(() => ArticleCg, (articlecgs) => articlecgs.conditionGeneral, {
+    eager: true,
+    cascade: true,
+    nullable: true,
+  })
+  @ApiProperty({ description: 'Liste des articles de la condition general' })
+  articlecgs?: ArticleCg[]
 }
