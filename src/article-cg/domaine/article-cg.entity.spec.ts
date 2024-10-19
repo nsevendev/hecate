de072f61c3792117f26736e1525b8e67e1d9c753
jha <haimezjohn@gmail.com>
2024-10-19 09:07:54 +0200

    update(13) : create entity article cg + test OK

--- src/article-cg/domaine/article-cg.entity.spec.ts
+++ src/article-cg/domaine/article-cg.entity.spec.ts
@@ -1,7 +1,16 @@
 import { ArticleCg } from './article-cg.entity'
 
 describe('ArticleCg', () => {
-  it('should be defined', () => {
+  it('ArtcileCg : est defini', () => {
     expect(new ArticleCg()).toBeDefined()
+  })
+
+  it("ArtcileCg : creation d'objet article cg et test des valeurs", () => {
+    const cg = new ArticleCg()
+    cg.title = 'titre'
+    cg.description = 'desc'
+
+    expect(cg.title).toBe('titre')
+    expect(cg.description).toBe('desc')
   })
 })
--- src/article-cg/domaine/article-cg.entity.ts
+++ src/article-cg/domaine/article-cg.entity.ts
@@ -1,9 +1,24 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
 import { ApiProperty } from '@nestjs/swagger'
+import { ConditionGeneral } from '../../condition-general/domaine/condition-general.entity'
 
 @Entity()
 export class ArticleCg {
   @PrimaryGeneratedColumn()
   @ApiProperty({ description: "id de l'article cg" })
   id: number
+
+  @Column()
+  @ApiProperty({ description: "Titre de l'article" })
+  title: string
+
+  @Column()
+  @ApiProperty({ description: "Sous titre de l'article" })
+  description: string
+
+  @ManyToOne(() => ConditionGeneral, (conditionGeneral) => conditionGeneral.articlecgs, {
+    onDelete: 'CASCADE',
+  })
+  @ApiProperty({ description: "Condition general de l'article" })
+  conditionGeneral: ConditionGeneral
 }
